97f3f0f369743e25a31cc2e2a27916551833403d
AbiSkywalker <97247313+AbiSkywalker@users.noreply.github.com>
2022-06-06 16:03:56 +0200

    Logs removed

--- auto_invoice_cron/models/sale.py
+++ auto_invoice_cron/models/sale.py
@@ -2,15 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-import logging
-_logger = logging.getLogger(__name__)
 
 class sale_order(models.Model):
     _inherit = 'sale.order'
 
     @api.model
     def _cron_auto_invoice_orders(self):
-        _logger.info("RUNNING AUTO INVOICE")
-        #crear facturas
+        #create invoices
         for so in self.env['sale.order'].search([('invoice_status', '=', 'to invoice')]):
             so._create_invoices()
